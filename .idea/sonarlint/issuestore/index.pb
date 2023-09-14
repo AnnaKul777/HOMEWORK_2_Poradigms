9b9a3d57ba232b2063a321c65e5dd5ff590d292d
AnnaKul <89269690212@mail.ru>
2023-09-14 18:05:08 +0300

    HOMEWORK 3 Poradigms

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+T
+$HomeWork_1/Zadacha1_and_ Zadacha2.py,f\a\fac716ff3bc04299a68383501ba9217a2ddb5793
---
+++ HomeWork_3/main.py
@@ -0,0 +1,6 @@
+from Zadacha import TicTacToe
+
+
+if __name__ == '__main__':
+    Game = TicTacToe()
+    Game.play()
